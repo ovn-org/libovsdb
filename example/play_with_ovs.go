ad0487fac6fc67ba6afd57c1380d49f3432f90c8
Madhu Venugopal <madhu@socketplane.io>
2014-11-14 04:41:13 -0800

    Updated example app with Caching
    Signed-off-by: Madhu Venugopal <madhu@socketplane.io>

--- example/play_with_ovs.go
+++ example/play_with_ovs.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"reflect"
 
 	"github.com/socketplane/libovsdb"
 )
@@ -12,17 +13,16 @@ import (
 
 var quit chan bool
 var update chan *libovsdb.TableUpdates
+var cache map[string]map[string]libovsdb.Row
 
 func play(ovs *libovsdb.OvsdbClient) {
-	ovs.MonitorAll("Open_vSwitch", "")
 	for {
 		select {
 		case currUpdate := <-update:
 			for table, tableUpdate := range currUpdate.Updates {
 				fmt.Println("Received Table update on : ", table)
 				if table == "Bridge" {
-					rows := tableUpdate.Rows
-					for uuid, row := range rows {
+					for uuid, row := range tableUpdate.Rows {
 						newRow := row.New
 						if _, ok := newRow.Fields["name"]; ok {
 							name := newRow.Fields["name"].(string)
@@ -39,15 +39,33 @@ func play(ovs *libovsdb.OvsdbClient) {
 
 }
 
+func populateCache(updates libovsdb.TableUpdates) {
+	for table, tableUpdate := range updates.Updates {
+		if _, ok := cache[table]; !ok {
+			cache[table] = make(map[string]libovsdb.Row)
+
+		}
+		for uuid, row := range tableUpdate.Rows {
+			empty := libovsdb.Row{}
+			if !reflect.DeepEqual(row.New, empty) {
+				cache[table][uuid] = row.New
+			} else {
+				delete(cache[table], uuid)
+			}
+		}
+	}
+}
+
 func main() {
 	quit = make(chan bool)
 	update = make(chan *libovsdb.TableUpdates)
+	cache = make(map[string]map[string]libovsdb.Row)
 
 	// By default libovsdb connects to 127.0.0.0:6400.
 	ovs, err := libovsdb.Connect("", 0)
 
 	// If you prefer to connect to OVS in a specific location :
-	//ovs, err := libovsdb.Connect("192.168.56.101", 6640)
+	// ovs, err := libovsdb.Connect("192.168.56.101", 6640)
 
 	if err != nil {
 		fmt.Println("Unable to Connect ", err)
@@ -55,6 +73,9 @@ func main() {
 	}
 	var notifier Notifier
 	ovs.Register(notifier)
+
+	initial, _ := ovs.MonitorAll("Open_vSwitch", "")
+	populateCache(*initial)
 
 	fmt.Println(`Silly game of stopping this app when a Bridge with name "stop" is monitored`)
 	go play(ovs)
@@ -65,6 +86,7 @@ type Notifier struct {
 }
 
 func (n Notifier) Update(context interface{}, tableUpdates libovsdb.TableUpdates) {
+	populateCache(tableUpdates)
 	update <- &tableUpdates
 }
 func (n Notifier) Locked([]interface{}) {
