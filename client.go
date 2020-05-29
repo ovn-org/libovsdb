ovn-org/libovsdb

commit b02798bdeab7bf94b988bdb57752d3887c7712ef

client: make "invalid Database Schema" message more informative
Tell us what database is having problems.

Signed-off-by: Dan Williams <dcbw@redhat.com>

client.go
@@ -250,7 +250,7 @@ func (ovs OvsdbClient) Transact(database string, operation ...Operation) ([]Oper
 	var reply []OperationResult
 	db, ok := ovs.Schema[database]
 	if !ok {
-		return nil, errors.New("invalid Database Schema")
+		return nil, errors.New("invalid Database %q Schema", database)
 	}
 
 	if ok := db.validateOperations(operation...); !ok {
@@ -269,7 +269,7 @@ func (ovs OvsdbClient) Transact(database string, operation ...Operation) ([]Oper
 func (ovs OvsdbClient) MonitorAll(database string, jsonContext interface{}) (*TableUpdates, error) {
 	schema, ok := ovs.Schema[database]
 	if !ok {
-		return nil, errors.New("invalid Database Schema")
+		return nil, errors.New("invalid Database %q Schema", database)
 	}
 
 	requests := make(map[string]MonitorRequest)

commit a5c512b13a659ea7921f5f3e13764e8fd7eb016f

client: Fix errors.New() arguments.
CI failed because of "too many arguments in call to errors.New".

Fixes: b02798bdeab ("client: make "invalid Database Schema" message more informative")
Signed-off-by: Han Zhou <hzhou8@ebay.com>

client.go
@@ -250,7 +250,7 @@ func (ovs OvsdbClient) Transact(database string, operation ...Operation) ([]Oper
 	var reply []OperationResult
 	db, ok := ovs.Schema[database]
 	if !ok {
-		return nil, errors.New("invalid Database %q Schema", database)
+		return nil, errors.New(fmt.Sprintf("invalid Database %q Schema", database))
 	}
 
 	if ok := db.validateOperations(operation...); !ok {
@@ -269,7 +269,7 @@ func (ovs OvsdbClient) Transact(database string, operation ...Operation) ([]Oper
 func (ovs OvsdbClient) MonitorAll(database string, jsonContext interface{}) (*TableUpdates, error) {
 	schema, ok := ovs.Schema[database]
 	if !ok {
-		return nil, errors.New("invalid Database %q Schema", database)
+		return nil, errors.New(fmt.Sprintf("invalid Database %q Schema", database))
 	}
 
 	requests := make(map[string]MonitorRequest)