871e9cf22279bba9d9d832cba7ca168f601cb912
Dave Tucker <dave@dtucker.co.uk>
2021-05-25 23:36:13 +0100

    ovsdb: Add Evaluate function to Conditions
    This allows for a Condition to be evaluated based
    on two supplied values.
    
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- ovsdb/condition.go
+++ ovsdb/condition.go
@@ -3,6 +3,7 @@ package ovsdb
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 )
 
 type ConditionFunction string
@@ -67,4 +68,134 @@ func (c Condition) UnmarshalJSON(b []byte) error {
 	}
 	c.Value = v[2]
 	return nil
+}
+
+// Evaluate will evaluate the condition on the two provided values
+// The conditions operately differently depending on the type of
+// the provided values. The behavjour is as described in RFC7047
+func (c ConditionFunction) Evaluate(a interface{}, b interface{}) (bool, error) {
+	x := reflect.ValueOf(a)
+	y := reflect.ValueOf(b)
+	if x.Kind() != y.Kind() {
+		return false, fmt.Errorf("comparison between %s and %s not supported", x.Kind(), y.Kind())
+	}
+	switch c {
+	case ConditionEqual:
+		return reflect.DeepEqual(a, b), nil
+	case ConditionNotEqual:
+		return !reflect.DeepEqual(a, b), nil
+	case ConditionIncludes:
+		switch x.Kind() {
+		case reflect.Slice:
+			return sliceContains(x, y), nil
+		case reflect.Map:
+			return mapContains(x, y), nil
+		case reflect.Int, reflect.Float64, reflect.Bool, reflect.String:
+			return reflect.DeepEqual(a, b), nil
+		default:
+			return false, fmt.Errorf("condition not supported on %s", x.Kind())
+		}
+	case ConditionExcludes:
+		switch x.Kind() {
+		case reflect.Slice:
+			return !sliceContains(x, y), nil
+		case reflect.Map:
+			return !mapContains(x, y), nil
+		case reflect.Int, reflect.Float64, reflect.Bool, reflect.String:
+			return !reflect.DeepEqual(a, b), nil
+		default:
+			return false, fmt.Errorf("condition not supported on %s", x.Kind())
+		}
+	case ConditionGreaterThan:
+		switch x.Kind() {
+		case reflect.Int:
+			return x.Int() > y.Int(), nil
+		case reflect.Float64:
+			return x.Float() > y.Float(), nil
+		case reflect.Bool, reflect.String, reflect.Slice, reflect.Map:
+		default:
+			return false, fmt.Errorf("condition not supported on %s", x.Kind())
+		}
+	case ConditionGreaterThanOrEqual:
+		switch x.Kind() {
+		case reflect.Int:
+			return x.Int() >= y.Int(), nil
+		case reflect.Float64:
+			return x.Float() >= y.Float(), nil
+		case reflect.Bool, reflect.String, reflect.Slice, reflect.Map:
+		default:
+			return false, fmt.Errorf("condition not supported on %s", x.Kind())
+		}
+	case ConditionLessThan:
+		switch x.Kind() {
+		case reflect.Int:
+			return x.Int() < y.Int(), nil
+		case reflect.Float64:
+			return x.Float() < y.Float(), nil
+		case reflect.Bool, reflect.String, reflect.Slice, reflect.Map:
+		default:
+			return false, fmt.Errorf("condition not supported on %s", x.Kind())
+		}
+	case ConditionLessThanOrEqual:
+		switch x.Kind() {
+		case reflect.Int:
+			return x.Int() <= y.Int(), nil
+		case reflect.Float64:
+			return x.Float() <= y.Float(), nil
+		case reflect.Bool, reflect.String, reflect.Slice, reflect.Map:
+		default:
+			return false, fmt.Errorf("condition not supported on %s", x.Kind())
+		}
+	default:
+		return false, fmt.Errorf("unsuported condition function %s", c)
+	}
+	// we should never get here
+	return false, fmt.Errorf("unreachable condition")
+}
+
+func sliceContains(x, y reflect.Value) bool {
+	for i := 0; i < y.Len(); i++ {
+		found := false
+		vy := y.Index(i)
+		for j := 0; j < x.Len(); j++ {
+			vx := x.Index(j)
+			if vy.Kind() == reflect.Interface {
+				if vy.Elem() == vx.Elem() {
+					found = true
+					break
+				}
+			} else {
+				if vy.Interface() == vx.Interface() {
+					found = true
+					break
+				}
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
+func mapContains(x, y reflect.Value) bool {
+	iter := y.MapRange()
+	for iter.Next() {
+		k := iter.Key()
+		v := iter.Value()
+		vx := x.MapIndex(k)
+		if !vx.IsValid() {
+			return false
+		}
+		if v.Kind() != reflect.Interface {
+			if v.Interface() != vx.Interface() {
+				return false
+			}
+		} else {
+			if v.Elem() != vx.Elem() {
+				return false
+			}
+		}
+	}
+	return true
 }
--- ovsdb/condition_test.go
+++ ovsdb/condition_test.go
@@ -1,6 +1,7 @@
 package ovsdb
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -127,6 +128,511 @@ func TestCondition_UnmarshalJSON(t *testing.T) {
 			}
 			if err := c.UnmarshalJSON(tt.args.b); (err != nil) != tt.wantErr {
 				t.Errorf("Condition.UnmarshalJSON() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestConditionFunctionEvaluate(t *testing.T) {
+	tests := []struct {
+		name    string
+		c       ConditionFunction
+		a       interface{}
+		b       interface{}
+		want    bool
+		wantErr bool
+	}{
+		{
+			"equal string true",
+			ConditionEqual,
+			"foo",
+			"foo",
+			true,
+			false,
+		},
+		{
+			"equal string false",
+			ConditionEqual,
+			"foo",
+			"bar",
+			false,
+			false,
+		},
+		{
+			"equal int true",
+			ConditionEqual,
+			1024,
+			1024,
+			true,
+			false,
+		},
+		{
+			"equal int false",
+			ConditionEqual,
+			1024,
+			2048,
+			false,
+			false,
+		},
+		{
+			"equal real true",
+			ConditionEqual,
+			float64(42.0),
+			float64(42.0),
+			true,
+			false,
+		},
+		{
+			"equal real false",
+			ConditionEqual,
+			float64(42.0),
+			float64(420.0),
+			false,
+			false,
+		},
+		{
+			"equal map true",
+			ConditionEqual,
+			map[string]string{"foo": "bar"},
+			map[string]string{"foo": "bar"},
+			true,
+			false,
+		},
+		{
+			"equal map false",
+			ConditionEqual,
+			map[string]string{"foo": "bar"},
+			map[string]string{"bar": "baz"},
+			false,
+			false,
+		},
+		{
+			"equal slice true",
+			ConditionEqual,
+			[]string{"foo", "bar"},
+			[]string{"foo", "bar"},
+			true,
+			false,
+		},
+		{
+			"equal slice false",
+			ConditionEqual,
+			[]string{"foo", "bar"},
+			[]string{"foo", "baz"},
+			false,
+			false,
+		},
+		{
+			"notequal string true",
+			ConditionNotEqual,
+			"foo",
+			"bar",
+			true,
+			false,
+		},
+		{
+			"notequal string false",
+			ConditionNotEqual,
+			"foo",
+			"foo",
+			false,
+			false,
+		},
+		{
+			"notequal int true",
+			ConditionNotEqual,
+			1024,
+			2048,
+			true,
+			false,
+		},
+		{
+			"notequal int false",
+			ConditionNotEqual,
+			1024,
+			1024,
+			false,
+			false,
+		},
+		{
+			"notequal real true",
+			ConditionNotEqual,
+			float64(42.0),
+			float64(24.0),
+			true,
+			false,
+		},
+		{
+			"notequal real false",
+			ConditionNotEqual,
+			float64(42.0),
+			float64(42.0),
+			false,
+			false,
+		},
+		{
+			"notequal map true",
+			ConditionNotEqual,
+			map[string]string{"foo": "bar"},
+			map[string]string{"bar": "baz"},
+			true,
+			false,
+		},
+		{
+			"notequal map false",
+			ConditionNotEqual,
+			map[string]string{"foo": "bar"},
+			map[string]string{"foo": "bar"},
+			false,
+			false,
+		},
+		{
+			"notequal slice true",
+			ConditionNotEqual,
+			[]string{"foo", "bar"},
+			[]string{"foo", "baz"},
+			true,
+			false,
+		},
+		{
+			"notequal slice false",
+			ConditionNotEqual,
+			[]string{"foo", "bar"},
+			[]string{"foo", "bar"},
+			false,
+			false,
+		},
+		{
+			"includes string true",
+			ConditionIncludes,
+			"foo",
+			"foo",
+			true,
+			false,
+		},
+		{
+			"includes string false",
+			ConditionIncludes,
+			"foo",
+			"bar",
+			false,
+			false,
+		},
+		{
+			"incldes int true",
+			ConditionIncludes,
+			1024,
+			1024,
+			true,
+			false,
+		},
+		{
+			"includes int false",
+			ConditionIncludes,
+			1024,
+			2048,
+			false,
+			false,
+		},
+		{
+			"includes real true",
+			ConditionIncludes,
+			float64(42.0),
+			float64(42.0),
+			true,
+			false,
+		},
+		{
+			"includes real false",
+			ConditionIncludes,
+			float64(42.0),
+			float64(420.0),
+			false,
+			false,
+		},
+		{
+			"includes map true",
+			ConditionIncludes,
+			map[interface{}]interface{}{1: "bar", "bar": "baz", "baz": "quux"},
+			map[interface{}]interface{}{1: "bar"},
+			true,
+			false,
+		},
+		{
+			"includes map false",
+			ConditionIncludes,
+			map[string]string{"foo": "bar", "bar": "baz", "baz": "quux"},
+			map[string]string{"quux": "foobar"},
+			false,
+			false,
+		},
+		{
+			"includes slice true",
+			ConditionIncludes,
+			[]string{"foo", "bar", "baz", "quux"},
+			[]string{"foo", "bar"},
+			true,
+			false,
+		},
+		{
+			"includes slice false",
+			ConditionIncludes,
+			[]string{"foo", "bar", "baz", "quux"},
+			[]string{"foobar", "quux"},
+			false,
+			false,
+		},
+		{
+			"excludes string true",
+			ConditionExcludes,
+			"foo",
+			"bar",
+			true,
+			false,
+		},
+		{
+			"excludes string false",
+			ConditionExcludes,
+			"foo",
+			"foo",
+			false,
+			false,
+		},
+		{
+			"excludes int true",
+			ConditionExcludes,
+			1024,
+			2048,
+			true,
+			false,
+		},
+		{
+			"excludes int false",
+			ConditionExcludes,
+			1024,
+			1024,
+			false,
+			false,
+		},
+		{
+			"excludes real true",
+			ConditionExcludes,
+			float64(42.0),
+			float64(24.0),
+			true,
+			false,
+		},
+		{
+			"excludes real false",
+			ConditionExcludes,
+			float64(42.0),
+			float64(42.0),
+			false,
+			false,
+		},
+		{
+			"excludes map true",
+			ConditionExcludes,
+			map[interface{}]interface{}{1: "bar", "bar": "baz", "baz": "quux"},
+			map[interface{}]interface{}{1: "foo"},
+			true,
+			false,
+		},
+		{
+			"excludes map false",
+			ConditionExcludes,
+			map[string]string{"foo": "bar", "bar": "baz", "baz": "quux"},
+			map[string]string{"foo": "bar"},
+			false,
+			false,
+		},
+		{
+			"excludes slice true",
+			ConditionExcludes,
+			[]string{"foo", "bar", "baz", "quux"},
+			[]string{"foobar"},
+			true,
+			false,
+		},
+		{
+			"excludes slice false",
+			ConditionExcludes,
+			[]string{"foobar", "bar", "baz", "quux"},
+			[]string{"foobar", "quux"},
+			false,
+			false,
+		},
+		{
+			"lt unsuported",
+			ConditionLessThan,
+			"foo",
+			"foo",
+			false,
+			true,
+		},
+		{
+			"lteq unsupported",
+			ConditionLessThanOrEqual,
+			[]string{"foo"},
+			[]string{"foo"},
+			false,
+			true,
+		},
+		{
+			"gt unsupported",
+			ConditionGreaterThan,
+			map[string]string{"foo": "foo"},
+			map[string]string{"foo": "foo"},
+			false,
+			true,
+		},
+		{
+			"gteq unsupported",
+			ConditionGreaterThanOrEqual,
+			true,
+			true,
+			false,
+			true,
+		},
+		{
+			"lt true",
+			ConditionLessThan,
+			0,
+			42,
+			true,
+			false,
+		},
+		{
+			"lteq true",
+			ConditionLessThanOrEqual,
+			42,
+			42,
+			true,
+			false,
+		},
+		{
+			"gt true",
+			ConditionGreaterThan,
+			float64(420.0),
+			float64(42.0),
+			true,
+			false,
+		},
+		{
+			"gteq true",
+			ConditionGreaterThanOrEqual,
+			float64(420.00),
+			float64(419.99),
+			true,
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.c.Evaluate(tt.a, tt.b)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ConditionFunction.Evaluate() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("ConditionFunction.Evaluate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSliceContains(t *testing.T) {
+	tests := []struct {
+		name string
+		a    interface{}
+		b    interface{}
+		want bool
+	}{
+		{
+			"string slice",
+			[]string{"foo", "bar", "baz"},
+			[]string{"foo", "bar"},
+			true,
+		},
+		{
+			"int slice",
+			[]int{1, 2, 3},
+			[]int{2, 3},
+			true,
+		},
+		{
+			"real slice",
+			[]float64{42.0, 42.0, 24.0},
+			[]float64{42.0, 24.0},
+			true,
+		},
+		{
+			"interface slice",
+			[]interface{}{1, "bar", "baz"},
+			[]interface{}{1, "bar"},
+			true,
+		},
+		{
+			"no match",
+			[]interface{}{1, "bar", "baz"},
+			[]interface{}{2, "bar"},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			x := reflect.ValueOf(tt.a)
+			y := reflect.ValueOf(tt.b)
+			if got := sliceContains(x, y); got != tt.want {
+				t.Errorf("compareSlice() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+func TestMapContains(t *testing.T) {
+	tests := []struct {
+		name string
+		a    interface{}
+		b    interface{}
+		want bool
+	}{
+		{
+			"string map",
+			map[string]string{"foo": "bar", "bar": "baz"},
+			map[string]string{"foo": "bar"},
+			true,
+		},
+		{
+			"int keys",
+			map[int]string{1: "bar", 2: "baz"},
+			map[int]string{1: "bar"},
+			true,
+		},
+		{
+			"interface keys",
+			map[interface{}]interface{}{1: 1024, 2: "baz"},
+			map[interface{}]interface{}{2: "baz"},
+			true,
+		},
+		{
+			"no key match",
+			map[string]string{"foo": "bar", "bar": "baz"},
+			map[string]string{"quux": "bar"},
+			false,
+		},
+		{
+			"no value match",
+			map[string]string{"foo": "bar", "bar": "baz"},
+			map[string]string{"foo": "quux"},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			x := reflect.ValueOf(tt.a)
+			y := reflect.ValueOf(tt.b)
+			if got := mapContains(x, y); got != tt.want {
+				t.Errorf("mapContains() = %v, want %v", got, tt.want)
 			}
 		})
 	}
