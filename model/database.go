d45fd33e23845932f1d9a5276524bbc3cc8e9b1b
Adrian Moreno <amorenoz@redhat.com>
2021-10-08 09:06:54 +0200

    database: Add 2-step initialization
    It is common to first create a DatabaseModel only based on the
    ClientDBModel, and then add / remove the schema to it when, e.g:
    when the client (re) connects.
    
    Validation is performed when the schema is added.
    
    Since the schema and mapper can be dynamically added and purged, protect
    them with a RWMutex
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- model/database.go
+++ model/database.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"sync"
+
 	"github.com/ovn-org/libovsdb/mapper"
 	"github.com/ovn-org/libovsdb/ovsdb"
 )
@@ -11,6 +13,7 @@ type DatabaseModel struct {
 	client *ClientDBModel
 	schema *ovsdb.DatabaseSchema
 	mapper *mapper.Mapper
+	mutex  sync.RWMutex
 }
 
 // NewDatabaseModel returns a new DatabaseModel
@@ -29,10 +32,49 @@ func (db *DatabaseModel) Client() *ClientDBModel {
 
 // Schema returns the DatabaseModel's schema
 func (db *DatabaseModel) Schema() *ovsdb.DatabaseSchema {
+	db.mutex.RLock()
+	defer db.mutex.RUnlock()
 	return db.schema
 }
 
 // Mapper returns the DatabaseModel's mapper
 func (db *DatabaseModel) Mapper() *mapper.Mapper {
+	db.mutex.RLock()
+	defer db.mutex.RUnlock()
 	return db.mapper
+}
+
+// NewPartialDatabaseModel returns a DatabaseModel what does not have a schema yet
+func NewPartialDatabaseModel(client *ClientDBModel) *DatabaseModel {
+	return &DatabaseModel{
+		client: client,
+	}
+}
+
+// Valid returns whether the DatabaseModel is fully functional
+func (db *DatabaseModel) Valid() bool {
+	db.mutex.RLock()
+	defer db.mutex.RUnlock()
+	return db.schema != nil
+}
+
+// SetSchema adds the Schema to the DatabaseModel making it valid if it was not before
+func (db *DatabaseModel) SetSchema(schema *ovsdb.DatabaseSchema) []error {
+	db.mutex.Lock()
+	defer db.mutex.Unlock()
+	errors := db.client.Validate(schema)
+	if len(errors) > 0 {
+		return errors
+	}
+	db.schema = schema
+	db.mapper = mapper.NewMapper(schema)
+	return errors
+}
+
+// ClearSchema removes the Schema from the DatabaseModel making it not valid
+func (db *DatabaseModel) ClearSchema() {
+	db.mutex.Lock()
+	defer db.mutex.Unlock()
+	db.schema = nil
+	db.mapper = nil
 }
