d5556fa2154d7fe9d84bdb9d3346d575cc152a76
Adrian Moreno <amorenoz@redhat.com>
2021-10-08 09:06:54 +0200

    database: Add 2-step initialization
    It is common to first create a DatabaseModel only based on the
    DatabaseModelRequest, and then add / remove the schema to it when, e.g:
    when the client (re) connects.
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- model/database.go
+++ model/database.go
@@ -8,6 +8,7 @@ import (
 // A DatabaseModel represents libovsdb's metadata about the database.
 // It's the result of combining the client's DatabaseModelRequest and the server's Schema
 type DatabaseModel struct {
+	valid   bool
 	request *DatabaseModelRequest
 	schema  *ovsdb.DatabaseSchema
 	mapper  *mapper.Mapper
@@ -35,4 +36,36 @@ func (db *DatabaseModel) Schema() *ovsdb.DatabaseSchema {
 // Mapper returns the DatabaseModel's mapper
 func (db *DatabaseModel) Mapper() *mapper.Mapper {
 	return db.mapper
+}
+
+// NewPartialDatabaseModel returns a DatabaseModel what does not have a schema yet
+func NewPartialDatabaseModel(request *DatabaseModelRequest) *DatabaseModel {
+	return &DatabaseModel{
+		valid:   false,
+		request: request,
+	}
+}
+
+// Valid returns whether the DatabaseModel is fully functional
+func (db *DatabaseModel) Valid() bool {
+	return db.valid
+}
+
+// SetSchema adds the Schema to the DatabaseModel making it valid if it was not before
+func (db *DatabaseModel) SetSchema(schema *ovsdb.DatabaseSchema) []error {
+	errors := db.request.Validate(schema)
+	if len(errors) > 0 {
+		return errors
+	}
+	db.schema = schema
+	db.mapper = mapper.NewMapper(schema)
+	db.valid = true
+	return errors
+}
+
+// ClearSchema removes the Schema from the DatabaseModel making it not valid
+func (db *DatabaseModel) ClearSchema() {
+	db.schema = nil
+	db.mapper = nil
+	db.valid = false
 }
