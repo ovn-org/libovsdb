cc339e8a3b9db372f429c51fe1b700ded5e088b0
Flavio Fernandes <flaviof@redhat.com>
2022-03-21 14:44:53 +0000

    cache: call OvsToNative on condition.Value before Function.Evaluate
    Fixes #299
    
    Signed-off-by: Flavio Fernandes <flaviof@redhat.com>

--- cache/cache.go
+++ cache/cache.go
@@ -383,7 +383,12 @@ func (r *RowCache) RowsByCondition(conditions []ovsdb.Condition) (map[string]mod
 				if err != nil {
 					return nil, err
 				}
-				ok, err := condition.Function.Evaluate(value, condition.Value)
+				tSchema := schema.Columns[condition.Column]
+				nativeValue, err := ovsdb.OvsToNative(tSchema, condition.Value)
+				if err != nil {
+					return nil, err
+				}
+				ok, err := condition.Function.Evaluate(value, nativeValue)
 				if err != nil {
 					return nil, err
 				}
