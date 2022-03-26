a20ed91ca2afc2ccc54b30a651862a479442fd9c
Flavio Fernandes <flaviof@redhat.com>
2022-03-26 10:45:18 +0000

    server: serialize handling of transactions
    While allowing other rpc handlers to run in parallel, this ovsdb server expects transactions
    to be serialized.
    
    Fixes #300
    
    Signed-off-by: Flavio Fernandes <flaviof@redhat.com>

--- server/server.go
+++ server/server.go
@@ -32,6 +32,7 @@ type OvsdbServer struct {
 	monitors     map[*rpc2.Client]*connectionMonitors
 	monitorMutex sync.RWMutex
 	logger       logr.Logger
+	txnMutex     sync.Mutex
 }
 
 // NewOvsdbServer returns a new OvsdbServer
@@ -188,6 +189,12 @@ func (o *OvsdbServer) NewTransaction(model model.DatabaseModel, dbName string, d
 
 // Transact issues a new database transaction and returns the results
 func (o *OvsdbServer) Transact(client *rpc2.Client, args []json.RawMessage, reply *[]ovsdb.OperationResult) error {
+	// While allowing other rpc handlers to run in parallel, this ovsdb server expects transactions
+	// to be serialized. The following mutex ensures that.
+	// Ref: https://github.com/cenkalti/rpc2/blob/c1acbc6ec984b7ae6830b6a36b62f008d5aefc4c/client.go#L187
+	o.txnMutex.Lock()
+	defer o.txnMutex.Unlock()
+
 	if len(args) < 2 {
 		return fmt.Errorf("not enough args")
 	}
